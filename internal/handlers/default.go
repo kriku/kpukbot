b3cfe088e1ff91116aad223b480c3bcb351b6749
Gosha Krikun <g.krikun@innopolis.ru>
2025-10-07 16:28:10 +0300

    fix: remove broken code

--- internal/handlers/default.go
+++ internal/handlers/default.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
-	"github.com/kriku/kpukbot/internal/repository/messages"
 )
 
 type DefaultHandler bot.HandlerFunc
@@ -14,8 +13,6 @@ type DefaultHandler bot.HandlerFunc
 func NewDefaultHandler(logger *slog.Logger) bot.HandlerFunc {
 	return func(ctx context.Context, b *bot.Bot, update *models.Update) {
 		logger.Info("Received update: %v", update)
-
-		messages.MessagesRepository.SaveMessage(ctx, update)
 
 		b.SendMessage(ctx, &bot.SendMessageParams{
 			ChatID: update.Message.Chat.ID,
